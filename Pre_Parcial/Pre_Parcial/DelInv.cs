65c3ec2c6254d4318a47d1fd1c54a2f1fdb69e4a
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-05-23 20:46:36 -0600

    Agregar DelInv.cs

---
+++ Pre_Parcial/Pre_Parcial/DelInv.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Pre_Parcial
+{
+    public partial class DelInv : UserControl
+    {
+        public DelInv()
+        {
+            InitializeComponent();
+        }
+    }
+}
