5c4747d5e0957c00061f18487050df49b8407612
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-05-23 19:31:21 -0600

    boton NewUser.cs

---
+++ Pre_Parcial/Pre_Parcial/NewUser.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Pre_Parcial
+{
+    public partial class NewUser : UserControl
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
