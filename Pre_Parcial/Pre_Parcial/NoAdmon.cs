613435cac86869cf8e7051dc9a2085a18ce2765f
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-05-23 19:16:48 -0600

    ventana Noadmon

---
+++ Pre_Parcial/Pre_Parcial/NoAdmon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pre_Parcial
+{
+    public partial class NoAdmon : UserControl
+    {
+        public NoAdmon()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
