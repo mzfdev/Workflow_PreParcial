8217275921aae1f7a4ee6b1db3946e98d97a8869
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-05-23 20:10:32 -0600

    Add ModInv.cs

---
+++ Pre_Parcial/Pre_Parcial/ModInv.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Pre_Parcial
+{
+    public partial class ModInv : UserControl
+    {
+        public ModInv()
+        {
+            InitializeComponent();
+        }
+    }
+}
