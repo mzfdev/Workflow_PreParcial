0dba82ae46b72786d60198ecbdcfad2400c85ffc
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-05-24 19:38:58 -0600

    LISTO ANADIR INVENTARIO

--- Pre_Parcial/Pre_Parcial/NewInv.cs
+++ Pre_Parcial/Pre_Parcial/NewInv.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Pre_Parcial
 {
@@ -7,6 +8,34 @@ namespace Pre_Parcial
         public NewInv()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Equals("") ||
+                textBox2.Equals(""))
+
+            {
+                MessageBox.Show("No puede dejar los campos vacios!");
+            }
+            else
+            {
+                try
+                {
+                    Conexion.realizarAccion($"INSERT INTO INVENTARIO(nombre_art,descripcion," +
+                                            $"precio,stock) VALUES(" +
+                                            $"'{textBox1.Text}'," +
+                                            $"'{textBox2.Text}'," +
+                                            $"{textBox3.Text}," +
+                                            $"{textBox4.Text})");
+
+                    MessageBox.Show("Articulo Añadido");
+                }
+                catch (Exception esg)
+                {
+                    MessageBox.Show("Ha ocurrido un error");
+                }
+            }
         }
     }
 }
